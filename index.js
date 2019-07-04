4259d8447334cdb0bfc84a1e9f0de8d19b3700d5
amlane <twelvexstring@gmail.com>
2019-07-03 23:10:57 -0500

    added up button

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 //j query to allow arrow to scroll down 1 page at a time
 $(document).ready(function() {
-  $(".arrow").click(function(event) {
-    $("html, body").animate({ scrollTop: "+=970vh" }, 800);
+  $(".down-arrow").click(function(event) {
+    $("html, body").animate({ scrollTop: "+=970px" }, 1000);
+  });
+});
+
+$(document).ready(function() {
+  $(".up-arrow").click(function(event) {
+    $("html, body").animate({ scrollTop: "+=-970px" }, 1000);
   });
 });
